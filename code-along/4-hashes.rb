7718a0dbcfb32067ae9993ce9816b5a78f45f101
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-01-24 22:04:23 +0000

    Creating hashes using Ruby

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,25 @@
 
 # Hashes are lists of *key-value pairs*
 
+profile = {
+    "name" => "Vindhya",
+    "location" => "Evanston",
+    "timeline" => [
+        {"status" => "Learning", "time" => "3:58 pm"},
+        {"status" => "Walking home" , "time" => "4:30 pm"}
+    ]
+}
+puts profile
+
 # Accessing data from the hash
 
+puts profile["name"]
+puts profile["location"]
+puts profile["status"]
+puts profile["timeline"][0]["status"]
+
 # More Complex Hashes
+
+profile["name"] = "Benjamin"
+profile["age"] = "..."
+puts profile.keys
