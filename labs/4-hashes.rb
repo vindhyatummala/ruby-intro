5b12dc5448298d03049fe7e32c943663871718c1
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-01-24 22:18:47 +0000

    Using bitcoin hash info to create a display output

--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -29,4 +29,15 @@ bitcoin = gets.chomp
 bitcoin = bitcoin.to_f
 
 # 3. inspect the bitcoin_data hash
-# puts bitcoin_data
+puts bitcoin_data.keys
+
+# 4. store the rate in USD of bitcoin
+rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+
+# 5. calculate the value of the user's bitcoin in USD
+value = rate * bitcoin
+puts value
+
+# 6. output the rate and value
+puts "1 Bitcoin is valued at $#{rate.round} USD."
+puts "Your Bitcoin is worth $#{value.round} USD."
