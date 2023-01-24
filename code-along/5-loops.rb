9723c8cfd6a79dddcf560a1a17c4b269bc7dbc15
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-01-24 22:32:40 +0000

    Using loops to write repetitive programs

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -3,9 +3,30 @@
 # ruby 5-loops.rb
 
 # Infinite Tacos -> Finite Tacos
-# loop do
-#   puts "tacos!"
-# end
+index = 0
+loop do
+    if index == 5
+        break
+    end
+  puts "tacos!"
+index = index + 1
+end
+puts "done"
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+index = 0
+loop do
+    if index == tacos.count
+        break
+    end
+    taco = tacos[index]
+
+  puts "#{taco} tacos!"
+index = index + 1
+end
+puts "done"
+
+for taco in tacos
+    puts "#{taco} tacos!"
+end
