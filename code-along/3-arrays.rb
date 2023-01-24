d100a63bb5f23b4e7a77a2b26d79d689d444e7a5
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-01-24 21:25:14 +0000

    Learning how to create arrays in Ruby

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,42 @@
 
 # Arrays are lists of things
 
+tasks = [
+    "make list",
+    "check off first todo",
+    "realize you've completed 2 todos",
+    "reward yourself with a nap"
+]
+puts tasks.inspect
+
+we_have_to_go_back = [4,8,15,16,23,42]
+puts we_have_to_go_back
+
+mixed_array = ["tacos", 3, true]
+puts mixed_array
+
+shopping_lists = [["diapers","wipes","toys"], ["coffee", "tea"]]
+puts shopping_lists
+
 # Accessing the array
+
+puts tasks [0]
+puts tasks [1]
+puts tasks [2]
+puts tasks [3]
+puts tasks [4]
+puts tasks [5]
+puts tasks [-1]
 
 # Add to the array
 
+tasks.append "wake up"
+puts tasks
+puts tasks + ["go back to bed"]
+
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
+
+puts tasks.count
+puts tasks.length
+puts tasks.size
