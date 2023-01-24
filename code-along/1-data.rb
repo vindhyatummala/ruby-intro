260e7415fda847ea948b0ddafd239ab5e5a2ee7f
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-01-24 20:21:07 +0000

    Using puts method in Ruby

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,14 +6,50 @@
 
 # Numbers
 
+puts 5
+puts 2
+puts 5+2
+puts 5-2
+puts 5*2
+# puts 5/2
+puts 5 % 2
+puts 5.0/2.0
+puts 5.0/2
+
 # Perform simple math with numbers
+
+puts 5*2+1
+puts 5*(2+1)
 
 # Strings
 
+puts Hello, World!
+puts "Hello, World!"
+
 # Combine strings together
+
+puts "Tacos are " + "delish"
+puts "Tacos"*3
+puts 3 * "Tacos"
+puts "Tacos: " + 3.to_s
 
 # Variables
 
+food = "Tacos"
+quantity = 3
+puts food*quantity
+
 # Combine strings and variables
 
+first_name = "Grogu"
+puts "Hello, " + first_name + "!"
+puts "Hello, #{first_name}!"
+greeting = "Hello, #{first_name}!"
+puts greeting
+puts "#{food}: #{quantity}"
+
 # String manipulation
+
+name = "Ruby"
+puts name.downcase
+puts name.upcase
